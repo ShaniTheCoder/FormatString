a068d8f2a0f122ba4aacaada03ad6c743c304233
shani <119065442+ShaniTheCoder@users.noreply.github.com>
2023-12-03 18:41:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
   format("Hello i am {0}, this is an Example of How you can use {1} ",
-         "ShaniTheCoder", "Format.h formate function");
+         "ShaniTheCoder", "Format.h format function");
 
   return 0;
 }
